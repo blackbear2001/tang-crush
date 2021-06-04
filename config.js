6707a449e7f8e42a4f7d416d31606f7e01fac943
blackbear2001 <85345648+blackbear2001@users.noreply.github.com>
2021-06-04 18:12:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Em bé Lam à!',
+    introDesc: `Hình như là có gì đó
+    không ổn lắm
+    em đang giận a đúng hơm
+    thôi a xin lỗi em bé Lam đừng giận a nhé`,
+    btnIntro: 'khoonggg !!!',
+    title: 'Em bé Lam tha cho a nha ',
+    desc: 'Yêu em bé Lam nhiều lắm ',
+    btnYes: 'Được rồi, tôi tha thứ cho anh',
+    btnNo: 'Không, còn lâu nhé',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Cám ơn em rất nhiều vì đã đến bên anh.',
+    btnAccept: 'Okiiiii em cũng yêu anh <3',
 }
